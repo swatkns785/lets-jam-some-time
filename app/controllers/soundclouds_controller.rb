b9a0566f372635dbac6097b262ee664018bdeed7
Steven Watkins <steven.watkins78@gmail.com>
2015-01-20 16:21:58 -0500

    Changed settings for production

--- app/controllers/soundclouds_controller.rb
+++ app/controllers/soundclouds_controller.rb
@@ -4,6 +4,6 @@ require 'soundcloud'
   def create
     client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_KEY'],
     :client_secret => ENV['SOUNDCLOUD_SECRET'],
-    :redirect_uri => 'http://localhost:3000/auth/soundcloud/callback')
+    :redirect_uri => 'http://agile-coast-1910.herokuapp.com/auth/soundcloud/callback')
   end
-  redirect_to client.authorize_url(http://localhost:3000)
+  redirect_to client.authorize_url(http://agile-coast-1910.herokuapp.com)
