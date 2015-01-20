b01c7ca8375a7f11cdef7013811ad911b55f61ea
Steven Watkins <steven.watkins78@gmail.com>
2015-01-20 18:02:34 -0500

    more configs again for heroku

--- app/controllers/soundclouds_controller.rb
+++ app/controllers/soundclouds_controller.rb
@@ -13,9 +13,9 @@ if Rails.env.production?
   :redirect_uri => 'https://agile-coast-1910.herokuapp.com/auth/soundcloud/callback')
 end
 
-
-if Rails.env.development? || Rails.env.test?
-  redirect_to client.authorize_url('http://localhost:3000')
-elsif Rails.env.production?
-  redirect_to client.authorize_url(:redirect_uri => 'https://agile-coast-1910.herokuapp.com/')
-end
+#
+# if Rails.env.development? || Rails.env.test?
+#   redirect_to client.authorize_url('http://localhost:3000')
+# elsif Rails.env.production?
+#   redirect_to client.authorize_url(:redirect_uri => 'https://agile-coast-1910.herokuapp.com/')
+# end
