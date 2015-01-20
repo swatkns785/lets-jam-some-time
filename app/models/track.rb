ab24fd75fae5c98a7008f95c6c9d5f6769c5e8f1
Steven Watkins <steven.watkins78@gmail.com>
2015-01-20 17:28:25 -0500

    more config

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,16 +1,28 @@
 class Track < ActiveRecord::Base
 
   def self.upload_to_soundcloud(track, upload_user)
+    if Rails.env.development? || Rails.env.test?
+      user = upload_user
 
-    user = upload_user
+      client = Soundcloud.new(client_id: ENV['DEV_SOUNDCLOUD_CLIENT_ID'], client_secret: ENV['DEV_SOUNDCLOUD_SECRET'], access_token: user.token)
 
-    client = Soundcloud.new(client_id: ENV['SOUNDCLOUD_CLIENT_ID'], client_secret: ENV['SOUNDCLOUD_SECRET'], access_token: user.token)
+      track = client.post('/tracks', :track => {
+        title: track[:track_title],
+        asset_data: File.new(track[:song_file].tempfile, 'rb') })
 
-    track = client.post('/tracks', :track => {
-      title: track[:track_title],
-      asset_data: File.new(track[:song_file].tempfile, 'rb') })
+      track.save
 
-    track.save
+    elsif Rails.env.production?
+      user = upload_user
+
+      client = Soundcloud.new(client_id: ENV['PROD_SOUNDCLOUD_CLIENT_ID'], client_secret: ENV['PROD_SOUNDCLOUD_SECRET'], access_token: user.token)
+
+      track = client.post('/tracks', :track => {
+        title: track[:track_title],
+        asset_data: File.new(track[:song_file].tempfile, 'rb') })
+
+      track.save
+    end
 
   end
 
