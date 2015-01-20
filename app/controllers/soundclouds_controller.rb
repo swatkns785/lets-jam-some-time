20e72903a41cd8db9c04ee351c0f7bb0eb7691d2
Steven Watkins <steven.watkins78@gmail.com>
2015-01-20 17:32:19 -0500

    more configs

--- app/controllers/soundclouds_controller.rb
+++ app/controllers/soundclouds_controller.rb
@@ -2,13 +2,13 @@ require 'soundcloud'
 
 
 if Rails.env.development? || Rails.env.test?
-  client = Soundcloud.new(:client_id => ENV['DEV_SOUNDCLOUD_KEY'],
+  client = Soundcloud.new(:client_id => ENV['DEV_SOUNDCLOUD_CLIENT_ID'],
   :client_secret => ENV['DEV_SOUNDCLOUD_SECRET'],
   :redirect_uri => 'http://localhost:3000/auth/soundcloud/callback')
 end
 
 if Rails.env.production?
-  client = Soundcloud.new(:client_id => ENV['PROD_SOUNDCLOUD_KEY'],
+  client = Soundcloud.new(:client_id => ENV['PROD_SOUNDCLOUD_CLIENT_ID'],
   :client_secret => ENV['PROD_SOUNDCLOUD_SECRET'],
   :redirect_uri => 'https://agile-coast-1910.herokuapp.com/auth/soundcloud/callback')
 end
