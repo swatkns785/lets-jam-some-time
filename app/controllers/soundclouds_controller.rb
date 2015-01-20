915c97ae18d97ea56ff89b0cba8aef4c8a2c34fd
Steven Watkins <steven.watkins78@gmail.com>
2015-01-20 17:55:46 -0500

    more configs

--- app/controllers/soundclouds_controller.rb
+++ app/controllers/soundclouds_controller.rb
@@ -17,5 +17,5 @@ end
 if Rails.env.development? || Rails.env.test?
   redirect_to client.authorize_url('http://localhost:3000')
 elsif Rails.env.production?
-  redirect_to client.authorize_url('https://agile-coast-1910.herokuapp.com/')
+  redirect_to client.authorize_url(:redirect_uri => 'https://agile-coast-1910.herokuapp.com/')
 end
