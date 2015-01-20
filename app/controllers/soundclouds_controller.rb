85883b7e8663811adcad6b7df642acf2dac31569
Steven Watkins <steven.watkins78@gmail.com>
2015-01-20 17:41:01 -0500

    More configs again

--- app/controllers/soundclouds_controller.rb
+++ app/controllers/soundclouds_controller.rb
@@ -14,8 +14,8 @@ if Rails.env.production?
 end
 
 
-if Rails.env.development? || Rails.env.test?
-  redirect_to client.authorize_url('http://localhost:3000')
-elsif Rails.env.production?
-  redirect_to client.authorize_url()
-end
+# if Rails.env.development? || Rails.env.test?
+#   # redirect_to client.authorize_url('http://localhost:3000')
+# elsif Rails.env.production?
+#   redirect_to client.authorize_url()
+# end
