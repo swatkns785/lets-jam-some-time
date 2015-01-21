490d3e34329e794ab3941d0eebe67afbe17b85c4
Steven Watkins <steven.watkins78@gmail.com>
2015-01-21 17:43:06 -0500

    hopefully this works

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,12 @@
+redirect_uri = if Rails.env.production?
+  'https://agile-coast-1910.herokuapp.com/auth/soundcloud/callback'
+else
+  'localhost:3000/auth/soundcloud/callback'
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider "soundcloud", ENV['SOUNDCLOUD_CLIENT_ID'], ENV['SOUNDCLOUD_SECRET']
+  provider "soundcloud",
+           ENV['SOUNDCLOUD_CLIENT_ID'],
+           ENV['SOUNDCLOUD_SECRET'],
+           client_options: {redirect_uri: redirect_uri }
 end
