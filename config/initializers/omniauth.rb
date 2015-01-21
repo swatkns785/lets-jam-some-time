f2b7c6a97179fdf6b4e191f0a3a5482dc7678dac
Steven Watkins <steven.watkins78@gmail.com>
2015-01-21 18:05:32 -0500

    hardcode more

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,5 +8,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider "soundcloud",
            ENV['SOUNDCLOUD_CLIENT_ID'],
            ENV['SOUNDCLOUD_SECRET'],
-           client_options: {redirect_uri: 'https://agile-coast-1910.herokuapp.com/auth/soundcloud/callback' }
+           client_options: {redirect_uri: ENV['SOUNDCLOUD_REDIRECT_URL'] }
 end
